522881b193a2d394f5c3eab570bb970baaa6c2a4
Wisse <wissehes@gmail.com>
2022-08-12 01:21:14 +0200

    Add Spotify client to the slayclient

--- src/Structs/SlayClient.ts
+++ src/Structs/SlayClient.ts
@@ -1,6 +1,8 @@
 import { AudioPlayer, createAudioPlayer } from "@discordjs/voice";
 import { PrismaClient } from "@prisma/client";
 import { Client, ClientOptions, Collection } from "discord.js";
+import SpotifyWebApi from "spotify-web-api-node";
+import { webServer } from "../web/web";
 import { Commands } from "../commands/Commands";
 import SlayCommand from "./SlayCommand";
 
@@ -9,6 +11,11 @@ export default class SlayClient extends Client {
     super(options);
     this.commands = new Collection();
     this.prisma = new PrismaClient();
+    this.spotify = new SpotifyWebApi({
+      clientId: process.env.SPOTIFY_CLIENTID,
+      clientSecret: process.env.SPOTIFY_CLIENTSECRET,
+      redirectUri: process.env.SPOTIFY_CALLBACK,
+    });
 
     this.players = new Collection();
     this.player = createAudioPlayer();
@@ -16,10 +23,14 @@ export default class SlayClient extends Client {
     for (const command of Commands) {
       this.commands.set(command.data.name, command);
     }
+
+    webServer(this);
   }
 
   commands: Collection<string, SlayCommand>;
   prisma: PrismaClient;
+  spotify: SpotifyWebApi;
+
   players: Collection<string, AudioPlayer>;
   player: AudioPlayer;
 }
