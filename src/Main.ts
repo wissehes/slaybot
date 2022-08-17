009ee0fce5c3b74a33dc8b368ead71023cd701b5
Wisse <wissehes@gmail.com>
2022-08-17 22:41:01 +0200

    Update bot status to now playing track and add event handlers for new interactions

--- src/Main.ts
+++ src/Main.ts
@@ -1,10 +1,32 @@
 import { ActivityType, GatewayIntentBits } from "discord.js";
 import SlayClient from "./Structs/SlayClient";
 import dotenv from "dotenv";
+import { WebSocket } from "ws";
+import { AzuraCastNowPlaying } from "./types/AzuraNPResponse";
+import createNowPlayingEmbed from "./helpers/createNPEmbed";
+import { CommandGroups } from "./commands/Commands";
+import createHelpEmbed, {
+  createMainHelpEmbed,
+} from "./helpers/createHelpEmbed";
 dotenv.config();
 
 const client = new SlayClient({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+});
+
+const ws = new WebSocket(process.env.WS_URL as string);
+
+ws.on("open", () => console.log("AzuraCast WebSocket connected!"));
+ws.on("message", (data) => {
+  const decoded: AzuraCastNowPlaying = JSON.parse(data.toString());
+  const np = decoded.now_playing.song.text;
+  try {
+    client.user?.setActivity(`${np} on SlayRadio`, {
+      type: ActivityType.Listening,
+    });
+  } catch (e) {
+    console.log("Couldn't update activity!");
+  }
 });
 
 client.once("ready", () => {
@@ -36,6 +58,32 @@ client.on("interactionCreate", async (interaction) => {
         });
       });
   }
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isSelectMenu()) return;
+
+  if (interaction.customId == "helpcategory") {
+    if (interaction.values[0] == "mainpage") {
+      const embed = createMainHelpEmbed();
+      interaction.update({ embeds: [embed] });
+      return;
+    }
+
+    const category = CommandGroups.find((g) => g.name == interaction.values[0]);
+    if (!category) return;
+
+    const embed = createHelpEmbed(category);
+    interaction.update({ embeds: [embed] });
+  }
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isButton()) return;
+  if (interaction.customId !== "reload-np") return;
+
+  const embed = await createNowPlayingEmbed();
+  interaction.update({ embeds: [embed] });
 });
 
 client.login(process.env.TOKEN);
